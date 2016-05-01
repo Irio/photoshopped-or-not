b7c319648af539634bbcd59753fa9511aed478a6
Irio Musskopf <iirineu@gmail.com>
2016-05-01 21:35:20 +0200

    Save images listed in CSV file

--- 1_save_urls.py
+++ 1_save_urls.py
@@ -1,19 +1,18 @@
-import ConfigParser
+import configparser
 import flickrapi
-import numpy as np
-import pandas as pd
 import os.path
+import pandas as pd
 
 def flickr_api(settings_path):
-    settings = ConfigParser.RawConfigParser()
+    settings = configparser.RawConfigParser()
     settings.read(settings_path)
     api_key = settings.get('Flickr', 'APIKey')
     secret = settings.get('Flickr', 'Secret')
     return flickrapi.FlickrAPI(api_key, secret)
 
 def load_dataset(file_path, file_headers):
-    if os.path.isfile(file_path):
-        return pd.read_csv(file_path, dtype=np.str)
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path)
     else:
         return pd.DataFrame(columns=file_headers)
 
@@ -23,13 +22,13 @@ keywords = 'manipulate,manipulation,manipulated,doctored,faked,photoshop,edited,
 flickr = flickr_api('config.ini')
 dataset = load_dataset(file_path, file_headers)
 
-for photo in flickr.walk(tag_mode='any', tags=keywords, per_page=10):
+for photo in flickr.walk(tag_mode='any', tags=keywords, safe_search=1):
     photo_id = photo.get('id')
-    if (dataset['photo_id'] == photo_id).any:
+    if dataset['photo_id'].isin([photo_id]).any():
         next
 
     available_sizes = flickr.photos.getSizes(photo_id=photo_id)[0]
-    url = available_sizes[len(available_sizes) - 1].get('source')
+    url = available_sizes[-1].get('source')
     row = pd.Series([photo_id, url], index=file_headers)
     dataset = dataset.append(row, ignore_index=True)
     dataset.to_csv(file_path, encoding='utf-8', index=False)
---
+++ 2_save_images.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import urllib.request
+import sys
+
+def download_image(url, file_path):
+    try:
+        urllib.request.urlretrieve(url, file_path)
+    except IOError as e:
+        print('%s %s %s' % (e[1], e[2], url), file=sys.stderr)
+
+remaining_downloads = 3500
+path = 'photoshopped'
+if not os.path.exists(path):
+    os.mkdir(path)
+
+dataset = pd.read_csv('images.csv')
+photo_ids = [name.split('_')[0] for name in os.listdir(path)]
+for index, values in dataset[~dataset['photo_id'].isin(photo_ids)].iterrows():
+    url = values[1]
+    file_path = '%s/%s' % (path, url.split('/')[-1])
+    if remaining_downloads == 0:
+        break
+    elif os.path.exists(file_path):
+        print('Skipping #%s' % values[0])
+        next
+
+    download_image(url, file_path)
+    remaining_downloads = remaining_downloads - 1
